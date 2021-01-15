91cc5571aa2994731c7f4dc9ba129035082be0a6
Egor Duvalov <happytosiu@gmail.com>
2021-01-15 12:39:50 +0300

    Initial commit

---
+++ src/main/java/com/QnA/Main.java
@@ -0,0 +1,4 @@
+package com.QnA;
+
+public class Main {
+}
---
+++ src/main/java/com/QnA/Steck.java
@@ -0,0 +1,9 @@
+package com.QnA;
+
+public class Steck <T> {
+    private Steck nextElement;
+    private Steck prevElement;
+    private T currElement;
+    private static int amount = 0;
+
+}
