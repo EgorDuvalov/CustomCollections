a0455812babaf775e52aabeba53f0e24e2a4adb5
Egor Duvalov <happytosiu@gmail.com>
2021-01-17 13:26:48 +0300

    exceptions now call super() wrong and zero are combined

--- src/main/java/com/Duvalov/Exceptions/WrongCapacityException.java
+++
@@ -1,7 +0,0 @@
-package com.Duvalov.Exceptions;
-
-public class WrongCapacityException extends Exception {
-    public WrongCapacityException(){
-        System.out.println("Capacity must be > 0");
-    }
-}
---
+++ src/main/java/com/innowise/duvalov/exception/IllegalCapacityValueException.java
@@ -0,0 +1,7 @@
+package com.innowise.duvalov.exception;
+
+public class IllegalCapacityValueException extends NegativeArraySizeException {
+    public IllegalCapacityValueException(){
+        super();
+    }
+}
