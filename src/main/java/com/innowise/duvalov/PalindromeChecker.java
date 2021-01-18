69b95e8d7179e8f9f35eaeaa854c8868fa533f41
Egor Duvalov <happytosiu@gmail.com>
2021-01-18 15:56:54 +0300

    PalindromeChecker created

---
+++ src/main/java/com/innowise/duvalov/PalindromeChecker.java
@@ -0,0 +1,11 @@
+package com.innowise.duvalov;
+
+public class PalindromeChecker<T> {
+    public boolean isPalindrome(T element){
+        return element.toString().equals(reverse(element.toString()));
+    }
+
+    public String reverse(String str){
+        return new StringBuilder(str).reverse().toString();
+    }
+}
